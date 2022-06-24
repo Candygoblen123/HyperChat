ddc2adde11ae2186258aff4d18326a9f31875405
Andrew Glaze <andrew.glaze76@gmail.com>
2022-06-24 11:15:13 -0400

    Update ci_post_clone.sh

--- Xcode/HyperChat/ci_scripts/ci_post_clone.sh
+++ Xcode/HyperChat/ci_scripts/ci_post_clone.sh
@@ -6,11 +6,9 @@
 #  Created by Andrew Glaze on 6/23/22.
 #  
 
+mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
 curl https://nodejs.org/download/release/latest-gallium/node-v16.15.1-darwin-x64.tar.xz --output node-16.tar.gz
-tar -xf node-16.tar.gz -C ./node-16/ --strip-components=1
-ls -al
-mkdir -p /Users/local/Homebrew/Cellar/node-16/
-cp -r node-16 /Users/local/Homebrew/Cellar/node-16/16
+tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
 brew link node-16
 
 brew install yarn
