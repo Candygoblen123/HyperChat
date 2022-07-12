acd63972428a037297c41ee9eb3a6e09d040f85a
Candygoblen123 <andrew.glaze76@gmail.com>
2022-07-12 17:48:47 -0400

    bump to mv3

--- Xcode/HyperChat/ci_scripts/ci_post_clone.sh
+++ Xcode/HyperChat/ci_scripts/ci_post_clone.sh
@@ -6,8 +6,5 @@
 #  Created by Andrew Glaze on 6/23/22.
 #  
 
-mkdir -p /Users/local/Homebrew/Cellar/node-16/16/
-curl https://nodejs.org/dist/v16.16.0/node-v16.16.0-darwin-x64.tar.gz --output node-16.tar.gz
-tar -xf node-16.tar.gz -C /Users/local/Homebrew/Cellar/node-16/16/ --strip-components=1
-brew link node-16
+brew install node@16
 brew install yarn
